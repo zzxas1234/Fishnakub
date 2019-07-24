b8ddaa297882f9fe71bbfca07f42307cbcd4760b
Ezilon <zzxas1234@hotmail.co.th>
2019-07-25 01:23:05 +1000

    .

---
+++ FishingVR/Assets/Game System/readText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class readText : MonoBehaviour
+{
+    public Text myText;
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+
+        string newString = randomText.FinalWord;
+        myText.text = newString;
+
+
+        
+
+    }
+}
